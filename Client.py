7656d6f78701c0abfdd17c001267cb1d0a3c128e
orico2007 <oricohen181107@gmail.com>
2024-08-06 22:52:28 +0300

    s

--- Client.py
+++ Client.py
@@ -1,8 +1,6 @@
-import socket
 import pygame
+import socket
 import io
-from pynput.keyboard import Listener as KeyboardListener, Key, KeyCode
-from pynput.mouse import Listener as MouseListener, Button
 
 def startClient():
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,52 +10,8 @@ def startClient():
     clientSocket.connect((host, port))
 
     pygame.init()
-
-    # Get the screen dimensions
-    info = pygame.display.Info()
-    screen_width, screen_height = info.current_w, info.current_h
-
-    screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Screen Viewer')
-
-    def on_press(key):
-        try:
-            key_str = key.char
-        except AttributeError:
-            key_str = str(key)
-        message = f"press|{key_str}\n"
-        clientSocket.send(message.encode())
-
-    def on_release(key):
-        try:
-            key_str = key.char
-        except AttributeError:
-            key_str = str(key)
-        message = f"release|{key_str}\n"
-        clientSocket.send(message.encode())
-        if key == Key.esc:
-            return False  # Stop the listener
-
-    def on_move(x, y):
-        message = f"move|{x},{y}|0,0\n"
-        clientSocket.send(message.encode())
-
-    def on_click(x, y, button, pressed):
-        action = "press" if pressed else "release"
-        button_str = 'left' if button == Button.left else 'right'
-        message = f"{action}|{x},{y}|{button_str}\n"
-        clientSocket.send(message.encode())
-
-    def on_scroll(x, y, dx, dy):
-        message = f"scroll|{x},{y}|{dx},{dy}\n"
-        clientSocket.send(message.encode())
-
-    # Start the keyboard and mouse listeners in separate threads
-    keyboard_listener = KeyboardListener(on_press=on_press, on_release=on_release)
-    keyboard_listener.start()
-
-    mouse_listener = MouseListener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
-    mouse_listener.start()
 
     while True:
         try:
